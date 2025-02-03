26490bac24783fee0be96ffd88bb9919c6072611
Eric Slutz <eric.slutz@icloud.com>
2025-02-03 15:02:54 -0500

    refactor footer component to use dynamic data from JSON; add footer interface and improve layout styles

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Footer } from './footer.interface';
+import data from '../../../assets/content.json';
+
 
 @Component({
-    selector: 'app-footer',
-    templateUrl: './footer.component.html',
-    styleUrls: ['./footer.component.css'],
-    standalone: false
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css'],
+  standalone: false
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  footer: Footer = data.footer;
+  currentYear: number = new Date().getFullYear();
 
+  ngOnInit(): void {
+  }
 }
