2444f95d7729cafd2d102a67be21ac4e9a20a6c8
Eric Slutz <eric.slutz@icloud.com>
2025-02-16 14:21:19 -0500

    add portfolio-api service and update components

---
+++ src/app/shared/services/portfolio-api.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, shareReplay } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PortfolioApiService {
+  private readonly apiUrl = '/api/getCosmosData';
+  private readonly cacheMap = new Map<string, Observable<any>>();
+
+  constructor(private http: HttpClient) {}
+
+  getComponentData<T>(component: string): Observable<T[]> {
+    if (!this.cacheMap.has(component)) {
+      const request = this.http
+        .post<T[]>(this.apiUrl, { component: component })
+        .pipe(
+          shareReplay({
+            bufferSize: 1,
+            windowTime: 24 * 60 * 60 * 1000, // 24 hours
+            refCount: true,
+          })
+        );
+      this.cacheMap.set(component, request);
+    }
+    return this.cacheMap.get(component)!;
+  }
+
+  clearCache(component?: string): void {
+    if (component) {
+      this.cacheMap.delete(component);
+    } else {
+      this.cacheMap.clear();
+    }
+  }
+}
