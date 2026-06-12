7562e6fba4d551ac51666b3f1762cbc0f2e6e472
Eric Slutz <eric.slutz@icloud.com>
2026-06-12 09:14:36 -0400

    Migrate API telemetry to OpenTelemetry

--- api/Program.cs
+++ api/Program.cs
@@ -1,12 +1,14 @@
 using System.Text.Json;
+using Azure.Monitor.OpenTelemetry.Exporter;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Azure.Functions.Worker.OpenTelemetry;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using OpenTelemetry;
 using PortfolioApi.Models;
 using PortfolioApi.Services;
 
@@ -14,10 +16,9 @@ var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
-// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
-builder.Services
-    .AddApplicationInsightsTelemetryWorkerService()
-    .ConfigureFunctionsApplicationInsights();
+builder.Services.AddOpenTelemetry()
+    .UseFunctionsWorkerDefaults()
+    .UseAzureMonitorExporter();
 
 builder.Services.AddOptions<CosmosConfiguration>()
   .Configure<IConfiguration>((options, config) =>
@@ -51,15 +52,5 @@ builder.Services.AddSingleton(serviceProvider =>
 builder.Services.AddSingleton<CmsAuthorizationService>();
 builder.Services.AddSingleton<HtmlContentSanitizer>();
 builder.Services.AddSingleton<MediaStorageService>();
-
-builder.Logging.Services.Configure<LoggerFilterOptions>(options =>
-{
-  LoggerFilterRule? defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName
-          == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
-  if (defaultRule is not null)
-  {
-    options.Rules.Remove(defaultRule);
-  }
-});
 
 builder.Build().Run();
